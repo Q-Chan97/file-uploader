ff3490f4cb5289bff5e4e9a7a410378c88d79400
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-25 12:19:24 -0500

    feat: Setup server and session

---
+++ app.js
@@ -0,0 +1,100 @@
+import path from "node:path";
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import { prisma } from "./db/prisma.js";
+import bcrypt from "bcryptjs";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+
+const app = express();
+
+const PORT = process.env.PORT;
+const __dirname = import.meta.dirname;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(
+    session({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        },
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: new PrismaSessionStore(
+            prisma,
+            {
+              checkPeriod: 2 * 60 * 1000,
+              dbRecordIdIsSessionId: true,
+              dbRecordIdFunction: undefined,
+            }
+        )
+    })
+);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.session());
+
+passport.use(
+    new LocalStrategy( async (username, password, done) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { username: username }
+            })
+            if (!user) {
+                return done(null, false, { message: "Profile not found" })
+            }
+
+            const match = await bcrypt.compare(password, user.password)
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" })
+            }
+
+            return done(null, user)
+        } catch (err) {
+            return done(err)
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: id }
+        })
+
+        if (!user) {
+            return done(null, false)
+        }
+
+        done(null, user)
+    } catch (err) {
+        done(err)
+    }
+});
+
+
+// Unmatched routes
+app.use((req, res) => {
+    res.status(404).render("404Page")
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.status || 500).render("404Page")
+});
+
+app.listen(PORT, (error) => {
+    if (error) throw error;
+    console.log(`Express server listening on port ${PORT}`)
+});
