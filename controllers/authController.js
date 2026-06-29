2c9874305077ff8c803a85c4ba9f2d1dc79a75b4
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-28 23:24:22 -0500

    feat: Build login get and signup get/post

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+import { prisma } from "../db/prisma.js";
+import bcrypt from "bcryptjs";
+import { createUser, createDefaultFolder } from "../db/queries.js";
+
+export async function getLogin (req, res) {
+    res.render("login")
+}
+
+export async function getSignUp (req, res) {
+    res.render("sign-up")
+}
+
+export async function postSignUp (req, res, next) {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        const newUser = await createUser(req.body.username, hashedPassword, req.body.email);
+        await createDefaultFolder(newUser.id);
+
+        res.redirect("/login")
+    } catch (err) {
+        next(err);
+    }
+}
