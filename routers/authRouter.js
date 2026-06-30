d1451e58c4ea0d7c599f75c3435845d81ba1f21c
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-29 20:34:53 -0500

    feat: Add authentication for login

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,10 +1,17 @@
 import Router  from "express";
+import passport from "passport";
 import { getLogin, getSignUp, postSignUp } from "../controllers/authController.js";
 import { validateUser, validateSignup } from "../utils/validation.js";
 
 const authRouter = Router();
 
 authRouter.get("/login", getLogin);
+authRouter.post("/login", 
+    passport.authenticate("local", {
+        successRedirect: "/home",
+        failureRedirect: "/login",
+        failureMessage: true,
+}));
 
 authRouter.get("/sign-up", getSignUp);
 authRouter.post("/sign-up", validateUser, validateSignup, postSignUp)
