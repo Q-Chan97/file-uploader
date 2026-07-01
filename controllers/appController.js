7a052f462b7a312006dd953ba97828661764e5c4
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-01 16:46:08 -0500

    feat: Implement getAllFiles and FolderView

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,4 +1,4 @@
-import { getUserFolders } from "../db/queries.js";
+import { getUserFolders, createNewFile, getAllUserFiles, getFolderById, getFilesByFolderId } from "../db/queries.js";
 
 export async function ensureAuthenticated (req, res, next) {
     if (req.isAuthenticated()) {
@@ -9,6 +9,37 @@ export async function ensureAuthenticated (req, res, next) {
 
 export async function getHome(req, res) {
     const folders = await getUserFolders(req.user.id);
+    const files = await getAllUserFiles(req.user.id);
 
-    res.render("home", {user: req.user, folders: folders})
+    res.render("home", {user: req.user, folders: folders, files: files})
+}
+
+export async function uploadFile(req, res) {
+    const folderId = req.body.folder;
+    await createNewFile(req.file, req.user.id, folderId);
+
+    console.log("folderId from form: ", folderId);
+    console.log("file", req.file)
+
+    if (folderId) {
+        res.redirect(`/folder/${folderId}`)
+    } else {
+        res.redirect("/")
+    }
+}
+
+export async function getFolderView(req, res) {
+    const folderId = parseInt(req.params.folderId);
+    console.log("folderId:", folderId)
+
+    const folders = await getUserFolders(req.user.id); // All folders for the sidebar
+    console.log("folders", folders)
+
+    const folder = await getFolderById(folderId); // Current folder for the view
+    console.log("folder", folder)
+
+    const folderFiles = await getFilesByFolderId(folderId);
+    console.log("Folder files: ", folderFiles)
+
+    res.render("folderView", {user: req.user, folders: folders, folder: folder, files: folderFiles})
 }
