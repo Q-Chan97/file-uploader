355f0387530a020f8701af4543459090c947893a
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-01 12:07:08 -0500

    feat: Implement auth forwarding and user folders

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,14 @@
+import { getUserFolders } from "../db/queries.js";
+
+export async function ensureAuthenticated (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login")
+}
+
 export async function getHome(req, res) {
-    res.render("home", {user: req.user})
+    const folders = await getUserFolders(req.user.id);
+
+    res.render("home", {user: req.user, folders: folders})
 }
