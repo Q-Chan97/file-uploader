d7e87816c81d44e8b41df7501a7dfd8cc60d7144
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-01 12:04:37 -0500

    feat: Change redirects, add logout path

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,19 +1,27 @@
 import Router  from "express";
 import passport from "passport";
-import { getLogin, getSignUp, postSignUp } from "../controllers/authController.js";
+import { getLogin, getSignUp, postSignUp, forwardAuthenticated } from "../controllers/authController.js";
 import { validateUser, validateSignup } from "../utils/validation.js";
 
 const authRouter = Router();
 
-authRouter.get("/login", getLogin);
+authRouter.get("/login", forwardAuthenticated, getLogin);
 authRouter.post("/login", 
     passport.authenticate("local", {
-        successRedirect: "/home",
+        successRedirect: "/",
         failureRedirect: "/login",
         failureMessage: true,
 }));
 
-authRouter.get("/sign-up", getSignUp);
+authRouter.get("/sign-up", forwardAuthenticated, getSignUp);
 authRouter.post("/sign-up", validateUser, validateSignup, postSignUp)
+
+authRouter.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+        if (err) return next(err);
+
+        res.redirect("/login");
+    });
+});
 
 export default authRouter;
