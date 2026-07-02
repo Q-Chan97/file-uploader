b0fd4be6ff3e2a4d8385e30eb9cf59420ecffc26
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-02 12:26:44 -0500

    feat: Implement file delete and download routes

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { getHome, uploadFile, ensureAuthenticated, getFolderView, createFolder } from "../controllers/appController.js";
+import { getHome, uploadFile, ensureAuthenticated, getFolderView, createFolder, deleteFile, downloadFile } from "../controllers/appController.js";
 
 import multer from "multer";
 const upload = multer({ dest: "uploads/" });
@@ -11,6 +11,10 @@ appRouter.post("/uploadFile", upload.single("newFile"), uploadFile);
 appRouter.get("/folder/:folderId", ensureAuthenticated, getFolderView);
 
 appRouter.post("/newFolder", createFolder);
+
+appRouter.post("/file/:fileId/delete", deleteFile);
+
+appRouter.get("/file/:fileId/download", downloadFile);
 
 appRouter.get("/", ensureAuthenticated, getHome);
 
