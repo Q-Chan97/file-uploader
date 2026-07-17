e236edc461442edf23b683bc319067a4180dd8a2
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-07-16 21:25:21 -0500

    feat: Update Supabase client w/ guard clause

---
+++ src/db/supabaseClient.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_SERVICE_KEY) {
+    throw new Error("SUPABASE_URL and SUPABASE_SERVICE_KEY must be set");
+}
+
+const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_SERVICE_KEY
+);
+
+export default supabase;
